c653d44f517e2a804c38731027af86f1d6c4af6c
Rafe Goldberg <rafegoldberg@gmail.com>
2018-06-13 10:09:32 -0700

    chore: update WP enqueue func

--- php/ui.php
+++ php/ui.php
@@ -4,10 +4,10 @@
   function LinkUiAssets(){
     $BUNDLE = get_template_directory_uri() . "/dist/BGLJ";
 
-    wp_enqueue_style('UiCSS',"$BUNDLE.css");
-    wp_enqueue_script('UiJS',"$BUNDLE.umd.js",[],false,true);
+    wp_enqueue_style(  'UiStyles', "$BUNDLE.css"  );
+    wp_enqueue_script( 'UiScript', "$BUNDLE.umd.min.js", [], false, true );
     
-    wp_localize_script('UiJS','WP_API_Settings',[
+    wp_localize_script('UiScript','WP_API_Settings',[
       'endpoint' => esc_url_raw(rest_url()),
       'nonce' => wp_create_nonce('wp_rest')
       ]);
